5d1199bb0c61923200532ae1fe012bbcb71ebb49
kimyoungi99 <kimyoungi99@naver.com>
2019-11-07 08:26:14 +0900

    no need to keep track of api_prac

--- api_prac/tmap_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-params = {"version": "1", "city":"서울", "county":"강남구","village":"도곡동"}
-headers = {"appKey": "54d398b9-d82b-4361-93ad-fb8366c44e77"}
-r = requests.get("http://apis.skplanetx.com/weather/current/hourly", params=params, headers=headers)
-print(r.json) #json형태로 출력
-
-
